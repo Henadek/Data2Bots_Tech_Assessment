ee009d64b31c26f0aed15249b9ba549881db113c
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2023-10-13 14:13:41 +0100

    refactored test cases to process schema files in folder

--- python_engineer_experienced_professional/schema_test.py
+++ python_engineer_experienced_professional/schema_test.py
@@ -2,29 +2,36 @@ import unittest
 import json
 import os
 
-f_path = os.path.join(os.path.dirname(__file__))
-with open(f'{f_path}/schema/schema_1.json') as fh:
-    output_data = json.load(fh)
+f_path = os.path.join(os.path.dirname(__file__), 'schema')
+f_path_list = [f'{f_path}/{i}' for i in os.listdir(f_path) if 'schema' in i]
+output_data = []
+for i in f_path_list:
+    with open(i) as fh:
+        output_data.append(json.load(fh))
 
 
 class MyTestCase(unittest.TestCase):
     def test_tag_padding(self):
-        for key in output_data:
-            if key != 'participantIds':
-                self.assertIn('tag', output_data[key])
+        for each in output_data:
+            for key in each:
+                if type(each[key]) is dict:
+                    self.assertIn('tag', each[key])
 
     def test_description_padding(self):
-        for key in output_data:
-            if key != 'participantIds':
-                self.assertIn('description', output_data[key])
+        for each in output_data:
+            for key in each:
+                if type(each[key]) is dict:
+                    self.assertIn('description', each[key])
 
     def test_required_padding(self):
-        for key in output_data:
-            if key != 'participantIds':
-                self.assertIn('required', output_data[key])
+        for each in output_data:
+            for key in each:
+                if type(each[key]) is dict:
+                    self.assertIn('required', each[key])
 
     def test_required_value(self):
-        for key in output_data:
-            if key != 'participantIds':
-                self.assertEqual(output_data[key]['required'], False)
+        for each in output_data:
+            for key in each:
+                if type(each[key]) is dict:
+                    self.assertEqual(each[key]['required'], False)
 
