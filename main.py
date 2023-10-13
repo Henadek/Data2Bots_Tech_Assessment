2441c5fc57d5297c0b90abf1c1a7fa09a37719fd
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2023-10-13 11:16:49 +0100

    read json data

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def read_data() -> json:
+    # TODO: Reads a JSON file similar to what's present in this location (./data/)
+    f_path = os.path.join(os.path.dirname(__file__), 'python_engineer_experienced_professional/data')
+    # print(os.listdir(f_path))
+
+    with open(f'{f_path}/data_1.json') as fh:
+        json_data = json.load(fh)
+
+    return json_data
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def parse_data(json_data) -> json:
+    # TODO: Sniffs the schema of the JSON file
+    print(json_data.keys())
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def output_data() -> json:
+    # TODO: Dumps the output in (./schema/)
+    pass
+
+
+
+json_data = read_data()
+# print(json_data)
+processed_data = parse_data(json_data)
+print(processed_data)
